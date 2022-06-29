5b34d6a5e45f500997496c7ce17cc8615a881163
manumasjoan <manumasjoan@gmail.com>
2022-06-28 22:46:54 -0300

    fixes

--- interpreter/src/main/java/interpreter/PrintlnResult.java
+++ interpreter/src/main/java/interpreter/PrintlnResult.java
@@ -5,12 +5,9 @@ public class PrintlnResult {
 
   public void addContent(String moreText) {
     content.append(moreText);
-    content.append("\n");
   }
 
   public String getContent() {
-    String contentString = content.toString();
-    contentString = contentString.substring(0, contentString.length() - 1);
-    return contentString;
+    return content.toString();
   }
 }
